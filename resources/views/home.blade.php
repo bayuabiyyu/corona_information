bc5d5f3b4aaa9ce47d18c9016fcc079386e3e35f
Bayu <ahmad.abiyyu1996@gmail.com>
2020-04-01 23:23:32 +0700

    Perubahan posisi tampilan di halaman utama

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,14 +62,6 @@
                         </ol>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Jumlah Pasien (Indonesia)</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="text-white stretched-link" href="#">{{ (int) str_replace(',', '', $response['data_total_indonesia'][0]['positif']) - ( (int)$response['data_total_indonesia'][0]['sembuh'] - (int)$response['data_total_indonesia'][0]['meninggal'] ) }}</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
                                 <div class="card bg-warning text-white mb-4">
                                     <div class="card-body">Total Pasien Positif (Indonesia)</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
@@ -90,6 +82,14 @@
                                     <div class="card-body">Total Pasien Meninggal (Indonesia)</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="text-white stretched-link" href="#">{{ $response['data_total_indonesia'][0]['meninggal'] }}</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Total Pasien Aktif (Indonesia)</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="text-white stretched-link" href="#">{{ (int)str_replace(',', '', $response['data_total_indonesia'][0]['positif']) -  (int)$response['data_total_indonesia'][0]['sembuh'] - (int)$response['data_total_indonesia'][0]['meninggal'] }}</a>
                                     </div>
                                 </div>
                             </div>
@@ -115,13 +115,13 @@
                                     <table class="table table-bordered table-hover" id="data_provinsi" width="100%" cellspacing="0">
                                     <caption>* Data diambil dari pihak ke-3 yang terintegrasi dengan kemenkes</caption>
                                         <thead>
-                                            <tr class="bg-dark text-white">
+                                            <tr class="bg-dark text-white text-center">
                                                 <th>No.</th>
                                                 <th>Provinsi</th>
                                                 <th>Positif</th>
                                                 <th>Sembuh</th>
                                                 <th>Meninggal</th>
-                                                <th>Jumlah</th>
+                                                <th>Pasien Aktif</th>
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -135,14 +135,16 @@
                                                 <td>{{ $item['attributes']['Kasus_Posi'] - $item['attributes']['Kasus_Semb'] - $item['attributes']['Kasus_Meni'] }}</td>
                                             </tr>
                                             @endforeach
-                                            <tr class="bg-dark text-white">
-                                                <td colspan="2" class="text-center">TOTAL</td>
-                                                <td>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Posi') ) }}</td>
-                                                <td>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Semb') ) }}</td>
-                                                <td>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Meni') ) }}</td>
-                                                <td>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Posi') ) - array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Semb') ) - array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Meni') ) }}</td>
-                                            </tr>
                                         </tbody>
+                                        <tfoot>
+                                            <tr class="bg-dark text-white">
+                                                <th colspan="2" class="text-center">TOTAL KESELURUHAN </th>
+                                                <th>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Posi') ) }}</th>
+                                                <th>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Semb') ) }}</th>
+                                                <th>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Meni') ) }}</th>
+                                                <th>{{ array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Posi') ) - array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Semb') ) - array_sum( array_column(array_column($response['data_provinsi'], 'attributes'), 'Kasus_Meni') ) }}</td>
+                                            </tr>
+                                        </tfoot>
                                     </table>
                                 </div>
                             </div>
@@ -165,9 +167,19 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="{{ asset('resources') }}/dist/js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('resources') }}/dist/assets/demo/chart-area-demo.js"></script>
-        <script src="{{ asset('resources') }}/dist/assets/demo/chart-bar-demo.js"></script>
+        {{-- <script src="{{ asset('resources') }}/dist/assets/demo/chart-area-demo.js"></script> --}}
+        {{-- <script src="{{ asset('resources') }}/dist/assets/demo/chart-bar-demo.js"></script> --}}
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+
+        <script>
+            $(document).ready(function(){
+                $('#data_provinsi').dataTable({
+                    "pageLength": 50
+                });
+            });
+
+        </script>
+
     </body>
 </html>
